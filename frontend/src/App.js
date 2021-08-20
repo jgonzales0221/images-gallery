2af6aeb91729c695cd4c27d8415c2be446d9e89e
jgonzales0221 <jogonz0221@gmail.com>
2021-08-19 21:16:23 -0600

    Images Col

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
-  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -19,7 +20,7 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        setImages([data, ...images]);
+        setImages([{ ...data, title: word }, ...images]);
       })
       .catch((err) => {
         console.log(err);
@@ -31,6 +32,15 @@ const App = () => {
     <div>
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col key={i} className="pb-3">
+              <ImageCard image={image} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
