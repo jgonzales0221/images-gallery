c43ad337fcca6642a297460c2ab488cdfea9cd19
jgonzales0221 <jogonz0221@gmail.com>
2021-08-21 13:38:57 -0600

    Welcome component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="secondary">
       <Container>
-        <Logo style={{ maxWidth: '20rem', maxHeight: '4rem' }} />
+        <Logo alt={title} style={{ maxWidth: '20rem', maxHeight: '4rem' }} />
       </Container>
     </Navbar>
   );
